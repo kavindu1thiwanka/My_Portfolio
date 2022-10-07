c9351bc06b58421775f9654f4d63e293d8aebf26
kavindu1thiwanka <kavidu12thiwanka@gmail.com>
2022-10-07 16:21:55 +0530

    POS - Customer Funtions Update

--- assignments/js/SPA/POS system Using Bootstrap/controller/CustomerController.js
+++ assignments/js/SPA/POS system Using Bootstrap/controller/CustomerController.js
@@ -1,11 +1,4 @@
-function loadCustomerData() {
-    for (var i of customers) {
-        var row = "<tr><td>" + i.id + "</td><td>" + i.name + "</td><td>" + i.address + "</td><td>" + i.salary + "</td></tr>";
-    }
-
-    $('#customerTable').append(row);
-}
-
+//Add Customer
 $('#addCustomerButton').click(function () {
 
     let customerID = $("#AddCustomerFormFile").val();
@@ -22,11 +15,81 @@ $('#addCustomerButton').click(function () {
 
     customers.push(customerObject);
 
-    loadCustomerData();
+    $("#AddCustomerFormFile").val("");
+    $("#AddCustomerFormFileMultiple").val("");
+    $("#AddCustomerFormFileDisabled").val("");
+    $("#AddCustomerFormFileSm").val("");
 
+    loadCustomerData();
 });
 
-$('#getAllCustomerDetails').click(function () {
-    $('#customerTable').clear();
-    loadCustomerData();
+//Search Customer
+function searchCustomer(cusID) {
+    for (let customer of customers) {
+        if (customer.id == cusID) {
+            return customer;
+        }
+    }
+    return null;
+}
+
+function setTextFieldValues(id, name, address, salary) {
+    $("#DeleteCustomerFormFile").val(id);
+    $("#DeleteCustomerFormFileMultiple").val(name);
+    $("#DeleteCustomerFormFileDisabled").val(address);
+    $("#DeleteCustomerFormFileSm").val(salary);
+}
+
+$('#customerSearchBtn').click(function (){
+    let typedId = $("#DeleteCustomerFormFile").val();
+    let customer = searchCustomer(typedId);
+    if (customer != null) {
+        setTextFieldValues(customer.id, customer.name, customer.address, customer.salary);
+    } else {
+        alert("There is no customer available for that " + typedId);
+        setTextFieldValues("", "", "", "");
+    }
 });
+
+//Delete Customer
+$('#customerDeleteBtn').click(function (){
+    let customerID = $("#DeleteCustomerFormFile").val();
+    let customer = searchCustomer(customerID);
+    if (customer != null) {
+        let option = confirm("Do you really want to delete customer id :" + customerID);
+        if (option){
+            let indexNumber = customers.indexOf(customer);
+            customers.splice(indexNumber, 1);
+            loadCustomerData();
+            alert("Customer Successfully Deleted..");
+            setTextFieldValues("", "", "", "");
+        }
+    } else {
+        alert("No such customer to delete. please check the id");
+    }
+});
+
+//Update Customer
+$('#customerUpdateBtn').click(function (){
+    let customerID = $("#DeleteCustomerFormFile").val();
+    let customer = searchCustomer(customerID);
+    if (customer != null) {
+        customer.id = $("#DeleteCustomerFormFile").val();
+        customer.name = $("#DeleteCustomerFormFileMultiple").val();
+        customer.address = $("#DeleteCustomerFormFileDisabled").val();
+        customer.salary = $("#DeleteCustomerFormFileSm").val();
+        loadCustomerData();
+        alert("Customer Updated Successfully");
+        setTextFieldValues("", "", "", "");
+    } else {
+        alert("Customer Updated Failed..!");
+    }
+});
+
+function loadCustomerData() {
+    $('#customerTable').empty();
+    for (var i of customers) {
+        var row = `<tr><td>${i.id}</td><td>${i.name}</td><td>${i.address}</td><td>${i.salary}</td></tr>`;
+        $('#customerTable').append(row);
+    }
+}
